12334f2b24a3eae3411255ace9234e4d6f97b7e4
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2024-02-02 21:36:47 +0100

    fix guideline

--- sirius/base.py
+++ sirius/base.py
@@ -1,6 +1,32 @@
-from typing import List
+from typing import Optional, List
 from dataclasses import dataclass
 
+
+@dataclass(frozen=True)
+class Message:
+    user: str
+    text: Optional[str] = None
+
+    def render(self):
+        result = self.user + ":"
+        if self.text is not None:
+            result += " " + self.text
+        return result
+
+
+@dataclass
+class Conversation:
+    messages: List[Message]
+
+    def prepare(self, message: Message):
+        self.messages.insert(0, message)
+        return self
+
+    def render(self):
+        return "<|endoftext|>".join(
+            [message.render() for message in self.messages]
+        )
+        
 
 @dataclass(frozen=True)
 class Guideline:
